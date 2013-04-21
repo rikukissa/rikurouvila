b5067c95ff25148a85708bb5eba8b4e6427e16ae
Riku Rouvila <riku.rouvila@gmail.com>
2013-04-22 02:15:28 +0300

    IE bugfixes + google analytics

--- public/js/modules/app.js
+++ public/js/modules/app.js
@@ -1,14 +1,25 @@
 (function() {
   require.config({
-    baseUrl: "js/modules",
-    paths: {
-      'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min'
-    }
+    baseUrl: "js/modules"
   });
 
   define(function(require) {
     return {
-      main: function() {}
+      main: function() {
+        var _gaq;
+
+        _gaq = [["_setAccount", "UA-31648732-1"], ["_setDomainName", "rikurouvila.fi"], ["_trackPageview"]];
+        return (function() {
+          var ga, s;
+
+          ga = document.createElement("script");
+          ga.type = "text/javascript";
+          ga.async = true;
+          ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+          s = document.getElementsByTagName("script")[0];
+          return s.parentNode.insertBefore(ga, s);
+        })();
+      }
     };
   });
 
--- src/coffee/app.coffee
+++ src/coffee/app.coffee
@@ -1,7 +1,23 @@
 require.config
   baseUrl: "js/modules"
-  paths: 
-    'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min'
+  #paths: 
+    #'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min'
         
 define (require) ->
   main: () ->
+    
+    # Google analytics
+    _gaq = [
+      ["_setAccount", "UA-31648732-1"], 
+      ["_setDomainName", "rikurouvila.fi"], 
+      ["_trackPageview"]
+    ]
+
+    (->
+      ga = document.createElement("script")
+      ga.type = "text/javascript"
+      ga.async = true
+      ga.src = ((if "https:" is document.location.protocol then "https://ssl" else "http://www")) + ".google-analytics.com/ga.js"
+      s = document.getElementsByTagName("script")[0]
+      s.parentNode.insertBefore ga, s
+    )()
